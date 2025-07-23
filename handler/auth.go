ce4f69c2116731a886ca2a052c4714b6bf6910f4
grbll@posteo.com <grbll@posteo.com>
2025-07-23 18:17:42 +0200

    modularized and change from html form to json

---
+++ handler/auth.go
@@ -0,0 +1,37 @@
+package handler
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type AuthHandler struct {
+	DB *sql.DB
+}
+
+func (h *AuthHandler) Login(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed!", http.StatusMethodNotAllowed)
+		return
+	}
+	var data struct {
+		Email string `json:"email"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+	}
+
+	var response struct {
+		Message string `json:"message"`
+	}
+
+	response.Message = fmt.Sprintf("Welcome %s", data.Email)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+	return
+}
